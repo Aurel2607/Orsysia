1f513f0b35ecf9ea2017cf9e33d9778e1a39870f
Aurel2607 <apajadon@hotmail.com>
2018-11-16 11:10:36 +0100

    Ajout librairie TinyXml

---
+++ tinyxml2-master/build/CMakeFiles/xmltest.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/xmltest.dir/xmltest.cpp.obj"
+  "xmltest.pdb"
+  "xmltest.exe"
+  "xmltest.exe.manifest"
+  "libxmltest.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/xmltest.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ tinyxml2-master/build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: E:/Perso/DevPerso/DevGame/tinyxml2-master/tinyxml2-master
+# Build directory: E:/Perso/DevPerso/DevGame/tinyxml2-master/tinyxml2-master/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(xmltest "E:/Perso/DevPerso/DevGame/tinyxml2-master/tinyxml2-master/build/xmltest.exe")
+set_tests_properties(xmltest PROPERTIES  WORKING_DIRECTORY "E:/Perso/DevPerso/DevGame/tinyxml2-master/tinyxml2-master/build")
