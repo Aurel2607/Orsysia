7124547dcecff17818fe69882891039fbcae9f4e
Aurel2607 <apajadon@hotmail.com>
2018-11-16 11:11:58 +0100

    Ajout Librairie Zlib

---
+++ zlib1211/zlib-1.2.11/build/CMakeFiles/zlibstatic.dir/DependInfo.cmake
@@ -0,0 +1,42 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "E:/Perso/DevPerso/DevGame/zlib1211/zlib-1.2.11/adler32.c" "E:/Perso/DevPerso/DevGame/zlib1211/zlib-1.2.11/build/CMakeFiles/zlibstatic.dir/adler32.obj"
+  "E:/Perso/DevPerso/DevGame/zlib1211/zlib-1.2.11/compress.c" "E:/Perso/DevPerso/DevGame/zlib1211/zlib-1.2.11/build/CMakeFiles/zlibstatic.dir/compress.obj"
+  "E:/Perso/DevPerso/DevGame/zlib1211/zlib-1.2.11/crc32.c" "E:/Perso/DevPerso/DevGame/zlib1211/zlib-1.2.11/build/CMakeFiles/zlibstatic.dir/crc32.obj"
+  "E:/Perso/DevPerso/DevGame/zlib1211/zlib-1.2.11/deflate.c" "E:/Perso/DevPerso/DevGame/zlib1211/zlib-1.2.11/build/CMakeFiles/zlibstatic.dir/deflate.obj"
+  "E:/Perso/DevPerso/DevGame/zlib1211/zlib-1.2.11/gzclose.c" "E:/Perso/DevPerso/DevGame/zlib1211/zlib-1.2.11/build/CMakeFiles/zlibstatic.dir/gzclose.obj"
+  "E:/Perso/DevPerso/DevGame/zlib1211/zlib-1.2.11/gzlib.c" "E:/Perso/DevPerso/DevGame/zlib1211/zlib-1.2.11/build/CMakeFiles/zlibstatic.dir/gzlib.obj"
+  "E:/Perso/DevPerso/DevGame/zlib1211/zlib-1.2.11/gzread.c" "E:/Perso/DevPerso/DevGame/zlib1211/zlib-1.2.11/build/CMakeFiles/zlibstatic.dir/gzread.obj"
+  "E:/Perso/DevPerso/DevGame/zlib1211/zlib-1.2.11/gzwrite.c" "E:/Perso/DevPerso/DevGame/zlib1211/zlib-1.2.11/build/CMakeFiles/zlibstatic.dir/gzwrite.obj"
+  "E:/Perso/DevPerso/DevGame/zlib1211/zlib-1.2.11/infback.c" "E:/Perso/DevPerso/DevGame/zlib1211/zlib-1.2.11/build/CMakeFiles/zlibstatic.dir/infback.obj"
+  "E:/Perso/DevPerso/DevGame/zlib1211/zlib-1.2.11/inffast.c" "E:/Perso/DevPerso/DevGame/zlib1211/zlib-1.2.11/build/CMakeFiles/zlibstatic.dir/inffast.obj"
+  "E:/Perso/DevPerso/DevGame/zlib1211/zlib-1.2.11/inflate.c" "E:/Perso/DevPerso/DevGame/zlib1211/zlib-1.2.11/build/CMakeFiles/zlibstatic.dir/inflate.obj"
+  "E:/Perso/DevPerso/DevGame/zlib1211/zlib-1.2.11/inftrees.c" "E:/Perso/DevPerso/DevGame/zlib1211/zlib-1.2.11/build/CMakeFiles/zlibstatic.dir/inftrees.obj"
+  "E:/Perso/DevPerso/DevGame/zlib1211/zlib-1.2.11/trees.c" "E:/Perso/DevPerso/DevGame/zlib1211/zlib-1.2.11/build/CMakeFiles/zlibstatic.dir/trees.obj"
+  "E:/Perso/DevPerso/DevGame/zlib1211/zlib-1.2.11/uncompr.c" "E:/Perso/DevPerso/DevGame/zlib1211/zlib-1.2.11/build/CMakeFiles/zlibstatic.dir/uncompr.obj"
+  "E:/Perso/DevPerso/DevGame/zlib1211/zlib-1.2.11/zutil.c" "E:/Perso/DevPerso/DevGame/zlib1211/zlib-1.2.11/build/CMakeFiles/zlibstatic.dir/zutil.obj"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_C
+  "NO_FSEEKO"
+  "_LARGEFILE64_SOURCE=1"
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "."
+  "../"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
